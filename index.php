f9d5b47b1c3de2a50e0f65c6e8679d55f2b0a185
scorpio21 <sonscorpio@gmail.com>
2025-09-02 15:39:34 +0200

    UI: modo oscuro estable en todas las páginas (sin bg-light), refuerzo CSS y cache-busting.

--- index.php
+++ index.php
@@ -140,6 +140,6 @@ if ($ultimos) {
 </div>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
-<script src="js/main.js"></script>
+<script src="js/main.js?v=20250902"></script>
 </body>
 </html>
--- pages/formulario.php
+++ pages/formulario.php
@@ -131,6 +131,6 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
   </form>
 </div>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
-<script src="../js/main.js"></script>
+<script src="../js/main.js?v=20250902"></script>
 </body>
 </html>
